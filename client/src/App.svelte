9cdfed4b450dc9ca5d2e2807186a68090404d264
Dan Cojocaru <dancojocaru2010@hotmail.com>
2021-11-14 15:13:40 +0200

    Initial commit

---
+++ client/src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+	import Card from "./Card.svelte";
+
+
+	export let name;
+</script>
+
+<main>
+	<h1 class="text-yellow-400">Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<Card title="Test" text="123"></Card>
+	<Card title="Test 2" text="Hello, world!"></Card>
+</main>
+
+<style global lang="postcss">
+	@tailwind base;
+	@tailwind components;
+	@tailwind utilities;
+</style>
